6411e5bb9a54ad7b7d7744b02692f7054967c98a
Giovanni D <giovanniyatze@gmail.com>
2019-12-19 00:29:08 -0600

    Add components folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
-import { NextPage } from 'next';
+import * as React from 'react'
+import Link from 'next/link'
+// import Layout from '../components/Layout'
+import { NextPage } from 'next'
 
 const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => {
   return (
