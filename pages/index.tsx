3073191cfe5faabe5af7cc14b3e95c83046141d2
Giovanni D <giovanniyatze@gmail.com>
2019-12-19 00:33:55 -0600

    Add fix in pages references and works in the run

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react'
-import Link from 'next/link'
+import * as React from 'react';
+import Link from 'next/link';
 // import Layout from '../components/Layout'
 import { NextPage } from 'next'
 
