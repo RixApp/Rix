536a5e93c7f0992ce970c2f26324f0b4b3dc8e82
Giovanni D <giovanniyatze@gmail.com>
2019-12-19 00:27:22 -0600

    Make a little tweak in index page to return multiple elements in the functional component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import { NextPage } from 'next';
 
-const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => <h1>Hello world! - user agent: {userAgent}</h1>;
+const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => {
+  return (
+    <h1>Hello world! - user agent: {userAgent}</h1>
+  )
+}
 
 Home.getInitialProps = async ({ req }) => {
   const userAgent = req ? req.headers['user-agent'] || '' : navigator.userAgent;
