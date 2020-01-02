5114b21b4a0a4e80d04e5dcc19b0c679d116af9e
Giovanni D <giovanniyatze@gmail.com>
2020-01-01 20:56:44 -0600

    Add pages initial definition

---
+++ pages/colors.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Link from 'next/link';
+import { NextPage } from 'next';
+
+const Colors: NextPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+Colors.getInitialProps = async ({ req }) => {
+  return 1;
+};
+
+export default Colors;
---
+++ pages/login.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Link from 'next/link';
+import { NextPage } from 'next';
+
+const Login: NextPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+Login.getInitialProps = async ({ req }) => {
+  return 1;
+};
+
+export default Login;
